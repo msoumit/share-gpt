5c5629b7a2cebfeacfeaeccdc4b71103736eb3b3
Soumit Mukherjee <mukherjee.soumit@gmail.com>
2026-03-06 21:14:23 -0600

    introduction message apostrophe change

--- ChatBot/src/webparts/chatBot/components/ChatIntroduction.tsx
+++ ChatBot/src/webparts/chatBot/components/ChatIntroduction.tsx
@@ -10,10 +10,10 @@ export const ChatIntroduction: React.FC = () => {
             <div className={styles.introductionContainer}>
                 <h2>Hello, <span>{currentUser.displayName ? currentUser.displayName : ""}</span></h2>
                 <div className={styles.introduction}>
-                    I&apos;m ShareGPT, your AI-powered assistant designed to help you with all your company-related questions. 
-                    Whether you&apos;re looking for IT support, HR information, or details from any other places, I&apos;ve got you covered. 
-                    Simply ask me about anything, and I&apos;ll do my best to guide you to the right information. 
-                    Let&apos;s make your workday easier and more efficient!
+                    I'm ShareGPT, your AI-powered assistant designed to help you with all your company-related questions. 
+                    Whether you're looking for IT support, HR information, or details from any other places, I've got you covered. 
+                    Simply ask me about anything, and I'll do my best to guide you to the right information. 
+                    Let's make your workday easier and more efficient!
                 </div>
             </div>  
         </div>
